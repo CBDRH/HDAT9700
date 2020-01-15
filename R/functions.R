cc6cc608dcf5e1387a6a810a4171500ce21b2975
Mark Hanly <m.hanly@unsw.edu.au>
2020-01-15 14:53:08 +1100

    Copied in individual tutorials and data

---
+++ R/functions.R
@@ -0,0 +1,97 @@
+# Functions for HDAT9700 Statistical Modelling II
+
+# The blocks below can be used to map chapters to tutorials
+# e.g. The chapter1() function is mapped to the tutorial "research-design-and-methods"
+# If mapping a chapter to a new tutorial make sure to update all the information for
+# that chapter in the relevant block below
+
+
+#' Chapter 1. Research Design and Methods
+#'
+#' Launch the tutorial for chapter 1
+#'
+#' @export
+chapter1 <- function() {
+  learnr::run_tutorial("research-design-and-methods", utils::packageName())
+}
+
+
+#' Chapter 2. Draw your assumptions before your conclusions
+#'
+#' Launch the tutorial for chapter 2
+#'
+#' @export
+chapter2 <- function() {
+  learnr::run_tutorial("test", utils::packageName())
+}
+
+
+#' Chapter 3. Propensity score matching
+#'
+#' Launch the tutorial for chapter 3
+#'
+#' @export
+chapter3 <- function() {
+  learnr::run_tutorial("propensity-score-matching", utils::packageName())
+}
+
+
+#' Chapter 4. Multilevel Modelling (Part I)
+#'
+#' Launch the tutorial for chapter 4
+#'
+#' @export
+chapter4 <- function() {
+  learnr::run_tutorial("multilevel-modelling-i", utils::packageName())
+}
+
+
+#' Chapter 5. Multilevel Modelling (Part II)
+#'
+#' Launch the tutorial for chapter 5
+#'
+#' @export
+chapter5 <- function() {
+  learnr::run_tutorial("multilevel-modelling-ii", utils::packageName())
+}
+
+
+#' Chapter 6. Growth Curve Modelling
+#'
+#' Launch the tutorial for chapter 6
+#'
+#' @export
+chapter6 <- function() {
+  learnr::run_tutorial("growth-curve-modelling", utils::packageName())
+}
+
+
+#' Chapter 7. Time Series Analysis
+#'
+#' Launch the tutorial for chapter 7
+#'
+#' @export
+chapter7 <- function() {
+  learnr::run_tutorial("time-series-analysis", utils::packageName())
+}
+
+
+#' Chapter 8. Interrupted Time Series Analysis
+#'
+#' Launch the tutorial for chapter 8
+#'
+#' @export
+chapter8 <- function() {
+  learnr::run_tutorial("interrupted-time-series-analysis", utils::packageName())
+}
+
+
+#' Chapter 9. Missing data and multiple imputation
+#'
+#' Launch the tutorial for chapter 9
+#'
+#' @export
+chapter9 <- function() {
+  learnr::run_tutorial("missing-data-and-multiple-imputation", utils::packageName())
+}
+
