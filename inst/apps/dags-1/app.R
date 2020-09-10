9c2f21099fb383c3200d46e8278618d79b5ff3bd
Mark Hanly <m.hanly@unsw.edu.au>
2020-09-10 12:56:08 +1000

    Updated shiny apps for DAGs tutorial

--- inst/apps/dags-1/app.R
+++ inst/apps/dags-1/app.R
@@ -23,6 +23,7 @@ ui <- fluidPage(
     titlePanel("Same three variables | Different assumptions | Different DAGS | Different models"),
 
     sidebarLayout(
+
         sidebarPanel(width=4,
             h3("Instructions"),
             p("The aim of this app is to illustrate how DAGS can be used to (i) represent assumptions about our data, and (ii) inform the selection of model variables."),
@@ -36,10 +37,10 @@ ui <- fluidPage(
             p("The data generation parameters are summarised below, and some of these can be adjusted. Importantly, there is one key difference between the two scenarios.
                 In Scenario A (the RCT), aspirin use is randomised, therefore there is no association between high blood pressure and aspirin use.
                 In Scenario B, there is an association between high blood pressure and aspirin use, reflecting that in an observational data setting,
-                people regularly taking aspirin may be doing so becuase they have an underlying risk factor like high blood pressure."),
+                people regularly taking aspirin may be doing so because they have an underlying risk factor like high blood pressure."),
 
             br(),
-            h4("Using the app can you demonstrate the following two points?"),
+            h4("Using the app, can you demonstrate the following two points?"),
 
             tags$ol(
                 tags$li("Using the dataset that mimics a randomised control trial, the estimated effect of aspirin use is the same, regardless of whether or not high blood pressure is
@@ -61,90 +62,113 @@ ui <- fluidPage(
             numericInput("hbp2pe", NULL,
                         value = 1.50, min = 1.0, max = 3.0, step = 0.5, width = '20%')
 
-        ),
+        ), # Closes sidebar panel
 
 
         mainPanel(width=8,
-            column(width = 6,
-                   h3("Scenario A: Randomised Control Trial"),
-                   h4("Data generation mechanism"),
-                   p("Scenario A reflects a randomised control trial exploring the association between aspirin use and blood pressure.
+            fluidRow(img(src='UNSW_2017_Big_Data_landscape.jpg', align = "right", height = '25%', width = '25%')),
+            fluidRow(
+                column(width = 6,
+                       h3("Scenario A: Randomised Control Trial"),
+                       h4("Data generation mechanism"),
+                       p("Scenario A reflects a randomised control trial exploring the causal effect of aspirin use on pulmonary embolism.
                             Participants have been randomised to receive antibiotics (treated) or a placebo pill (control)"),
-                   br(),
-                   p("The DAG below represents this data generation mechanism. Because aspirin use is under controlled of the researchers,
-                            and randomised to a large number of subjects, we can assume there is causal effect of high blood pressure on aspirin usage."),
-                   hr(),
-                   tabsetPanel(
-                       tabPanel(title = "Can you draw the DAG?", icon = icon("pen-nib"),
-                                helpText("Try drawing a DAG to represent the assumptions about the variables under this scenario. Your DAG should include the
+                       br(),
+                       p(HTML(paste0("The DAG below represents this data generation mechanism (you should try drawing it before revealing the solution). In this scenario,
+                       aspirin use is controlled by the researchers, and randomised to a large number of subjects. Aspirin use decreases the risk of
+                                     pulmonary embolism (tx", "&rarr;", "pe); and high blood pressure increases the risk of pulmonary embolism (hbp", "&rarr;", "pe)."))),
+
+                ),
+
+                column(width = 6,
+                       h3("Scenario B: Observational data"),
+                       h4("Data generation mechanism"),
+                       p("Scenario B reflects an observational data analysis where assignment to the exposure of interest (aspirin use) is not under the control of the researchers."),
+                       br(),
+                       p(HTML(paste0("The data are generated to reflect this: high blood pressure increases the likelihood of aspirin use (hbp", "&rarr;", "tx); aspirin use decreases the risk of
+                                     pulmonary embolism (tx", "&rarr;", "pe); and high blood pressure increases the risk of pulmonary embolism (hbp", "&rarr;", "pe). The strength of these relationships
+                                     underlying the data generation mechanism can be controlled in the panel on the left." )))
+                )
+                ), # Closes fluid row
+
+
+                br(),
+                hr(),
+
+                fluidRow(
+                    column(width = 6,
+                           tabsetPanel(
+                               tabPanel(title = "Can you draw the DAG?", icon = icon("pen-nib"),
+                                        helpText("Try drawing a DAG to represent the assumptions about the variables under this scenario. Your DAG should include the
                                 three variables: aspirin use (exposure), pulmonary embolism (outcome) and high blood pressure (covariate).")),
-                       tabPanel("See the solution", icon = icon("project-diagram"),
-                                fluidRow(
-                                    column(width=7,
-                                           plotOutput("dag1", width = '100%')
-                                    ),
-                                    column(width=5, br(),
-                                           helpText("This DAG is consistent with the data generation mechansism under Scenario A (reflecting a RCT).
+                               tabPanel("See the solution", icon = icon("project-diagram"),
+                                        fluidRow(
+                                            column(width=7,
+                                                   plotOutput("dag1", width = '100%')
+                                            ),
+                                            column(width=5, br(),
+                                                   helpText("This DAG is consistent with the data generation mechansism under Scenario A (reflecting a RCT).
                                      Because aspirin use is randomised, we know that in a large sample there is no relationship between high blood pressure & aspirin use.
                                      Accordingly, there is no arrow from hbp to tx."))
-                                )
-                                ),
-                       tabPanel(title = "View model estimates", icon = icon("table"),
-                                h4("Covariate selection:"),
-                                checkboxInput("inclhbp1", "Control for high blood pressure?", value = FALSE),
-                                h4("Model summary:"),
-                                uiOutput("eq1"),
-                                h4("Estimated parameters:"),
-                                htmlOutput("tab1")
-                       ),
-                       tabPanel(title = "View raw data", icon = icon("file-excel"),
-                                DT::dataTableOutput("data1"))
-                   )
-            ),
+                                        )
+                               ),
+                               tabPanel(title = "View model estimates", icon = icon("table"),
+                                        h4("Covariate selection:"),
+                                        checkboxInput("inclhbp1", "Control for high blood pressure?", value = FALSE),
+                                        h4("Model summary:"),
+                                        uiOutput("eq1"),
+                                        h4("Estimated parameters:"),
+                                        htmlOutput("tab1")
+                               ),
+                               tabPanel(title = "View raw data", icon = icon("file-excel"),
+                                        DT::dataTableOutput("data1"))
+                           )
+                           ), # Closes first column
 
-            column(width = 6,
-                   h3("Scenario B: Observational data"),
-                   h4("Data generation mechanism"),
-                   p("Pr(pe = 1 | tx = 0) = 0.1"),
-                   p("Scenario A reflects a randomised control trial exploring the association between aspirin use and blood pressure.
-                            Participants have been randomised to receive antibiotics (treated) or a placebo pill (control)"),
-
-                   hr(),
-                   tabsetPanel(
-                       tabPanel(title = "Can you draw the DAG?", icon = icon("pen-nib"),
-                                helpText("Try drawing a DAG to represent the assumptions about the variables under this scenario. Your DAG should include the
+                    column(width = 6,
+                           tabsetPanel(
+                               tabPanel(title = "Can you draw the DAG?", icon = icon("pen-nib"),
+                                        helpText("Try drawing a DAG to represent the assumptions about the variables under this scenario. Your DAG should include the
                                 three variables: aspirin use (exposure), pulmonary embolism (outcome) and high blood pressure (covariate).")),
-                       tabPanel("See the solution", icon = icon("project-diagram"),
-                                fluidRow(
-                                    column(width=7,
-                                           plotOutput("dag2", width = '100%')
-                                    ),
-                                    column(width=5, br(),
-                                           helpText(HTML(paste0("This DAG is consistent with the data generation mechanism under Scenario B (observational data).
+                               tabPanel("See the solution", icon = icon("project-diagram"),
+                                        fluidRow(
+                                            column(width=7,
+                                                   plotOutput("dag2", width = '100%')
+                                            ),
+                                            column(width=5, br(),
+                                                   helpText(HTML(paste0("This DAG is consistent with the data generation mechanism under Scenario B (observational data).
                                        We know there is a relationship between high blood pressure & aspirin use so the DAG includes an arrow from hbp to tx.
-                                       This results in a backdoor path between our treatment tx and exposure pe (the path tx", "&rarr;", "hbp", "&rarr;", "pe).",
-                                                                br(), br(), "This backdoor path will introduce bias on our estimate of the effect of aspirin use
+                                       This results in a backdoor path between our treatment tx and exposure pe (the path tx", "&larr;", "hbp", "&rarr;", "pe).",
+                                                                        br(), br(), "This backdoor path will introduce bias on our estimate of the effect of aspirin use
                                                    on pulmonary embolism (tx", "&rarr;", "pe). To avoid this bias we must close the backdoor path by
                                                    controlling for high blood pressure.")))
-                                           )
-                                )
-                            ),
-                       tabPanel(title = "View model estimates", icon = icon("table"),
-                                h4("Covariate selection:"),
-                                checkboxInput("inclhbp2", "Control for high blood pressure?", value = FALSE),
-                                h4("Model summary:"),
-                                uiOutput("eq2"),
-                                h4("Estimated parameters:"),
-                                htmlOutput("tab2")
-                            ),
-                       tabPanel(title = "View raw data", icon = icon("file-excel"),
-                                DT::dataTableOutput("data2"))
-                   )
-                   )
+                                            )
+                                        )
+                               ),
+                               tabPanel(title = "View model estimates", icon = icon("table"),
+                                        h4("Covariate selection:"),
+                                        checkboxInput("inclhbp2", "Control for high blood pressure?", value = FALSE),
+                                        h4("Model summary:"),
+                                        uiOutput("eq2"),
+                                        h4("Estimated parameters:"),
+                                        htmlOutput("tab2")
+                               ),
+                               tabPanel(title = "View raw data", icon = icon("file-excel"),
+                                        DT::dataTableOutput("data2"))
+                           )
+                           ) # Closes second column
 
-        )
-    )
-)
+                ), # Closes fluid row
+            br(),
+            fluidRow(
+                tags$blockquote("The key point to recognise here is that you can have the same research question and the same potential model covariates
+                                available, but the specifying the correct model to answer the causal question at hand depends on understanding the causal
+                                relationships between the variables")
+            )
+
+        ) # closes main panel
+    ) # closes sidebar layout
+) # closes fluid page
 
 
 #########################################################
